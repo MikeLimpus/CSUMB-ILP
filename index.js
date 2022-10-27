b07a5779713fd1542b3aecdee2ff8dda19ba79ff
Mike Limpus <mikelimpus111@gmail.com>
2022-10-26 19:16:44 -0700

    Add helmet middleware

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const helmet = require("helmet");
+
 const courseInfo = require("./course_info.json");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(helmet())
 console.log(courseInfo.courses.cst300);
 app.get("/", (req, res) => {
     res.render("index");
