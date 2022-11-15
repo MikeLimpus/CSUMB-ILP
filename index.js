63e3f4f181daf8dbc6f2f78582b8d31163764ea2
Mike Limpus <mikelimpus111@gmail.com>
2022-11-14 17:04:54 -0800

    Added all course descriptions and projects up to CST328

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const courseInfo = require("./course_info.json");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 //app.use(helmet())
-console.log(courseInfo.courses.cst300);
 app.get("/", (req, res) => {
     res.render("index");
 });
@@ -21,47 +20,47 @@ app.get("/cst338", (req, res) => {
 })
 
 app.get("/cst363", (req, res) => {
-    res.render("template", {"courseInfo": courseInfo.courses.default}) 
+    res.render("template", {"courseInfo": courseInfo.courses.cst363}) 
 })
 
 app.get("/cst334", (req, res) => {
-    res.render("template", {"courseInfo": courseInfo.courses.default}) 
+    res.render("template", {"courseInfo": courseInfo.courses.cst334}) 
 })
 
 app.get("/cst311", (req, res) => {
-    res.render("template", {"courseInfo": courseInfo.courses.default}) 
+    res.render("template", {"courseInfo": courseInfo.courses.cst311}) 
 })
 
 app.get("/cst336", (req, res) => {
-    res.render("template", {"courseInfo": courseInfo.courses.default}) 
+    res.render("template", {"courseInfo": courseInfo.courses.cst336}) 
 })
 
 app.get("/cst462", (req, res) => {
-    res.render("template", {"courseInfo": courseInfo.courses.default}) 
+    res.render("template", {"courseInfo": courseInfo.courses.cst462}) 
 })
 
 app.get("/cst328", (req, res) => {
-    res.render("template", {"courseInfo": courseInfo.courses.default}) 
+    res.render("template", {"courseInfo": courseInfo.courses.cst328}) 
 })
 
 app.get("/cst370", (req, res) => {
-    res.render("template", {"courseInfo": courseInfo.courses.default}) 
+    res.render("template", {"courseInfo": courseInfo.courses.cst370}) 
 })
 
 app.get("/cst383", (req, res) => {
-    res.render("template", {"courseInfo": courseInfo.courses.default}) 
+    res.render("template", {"courseInfo": courseInfo.courses.cst383}) 
 })
 
 app.get("/cst329", (req, res) => {
-    res.render("template", {"courseInfo": courseInfo.courses.default}) 
+    res.render("template", {"courseInfo": courseInfo.courses.cst329}) 
 })
 
 app.get("/cst438", (req, res) => {
-    res.render("template", {"courseInfo": courseInfo.courses.default}) 
+    res.render("template", {"courseInfo": courseInfo.courses.cst438}) 
 })
 
 app.get("/cst499", (req, res) => {
-    res.render("template", {"courseInfo": courseInfo.courses.default}) 
+    res.render("template", {"courseInfo": courseInfo.courses.cst499}) 
 })
 
 app.listen(port, () => {
